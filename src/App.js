cfe830345059e4ae35d9c7843ccbcc0d40c48b64
Heri Utomo <heri.utomo@asabri.co.id>
2019-01-25 23:05:13 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -4,46 +4,50 @@ import { AuthenticationContext } from 'react-adal';
 import './App.css';
 
 class App extends Component {
-  componentDidMount() {
-    const aadTenant = 'asabri.co.id';
-    const spaClientId = '1e957e0a-8aa7-4f77-91d7-27d25be9909b'; //AAD app client id for this app
-    const serviceClientId = '1fd046c7-83dd-4c06-bbff-546ff92232b0'; //AAD app client id for the service API app
-    const serviceUrl = 'http://api-hrms.asabri.co.id/api/pegawai'; // the service API endpoint
+  constructor() {
+    super();
+    this.state = {
+      aadTenant: 'asabri.co.id',
+      spaClientId: '1e957e0a-8aa7-4f77-91d7-27d25be9909b', //AAD app client id for this app
+      serviceClientId: '1fd046c7-83dd-4c06-bbff-546ff92232b0', //AAD app client id for the service API app
+      serviceUrl: 'http://api-hrms.asabri.co.id/api/pegawai', // the service API endpointa
+    }
+  }
 
+  componentDidMount() {
     const authContext = new AuthenticationContext({
       instance: 'https://login.microsoftonline.com/',
-      tenant: aadTenant,
-      clientId: spaClientId,
+      tenant: this.state.aadTenant,
+      clientId: this.state.serviceClientId,
       postLogoutRedirectUri: window.location.origin,
       cacheLocation: 'localStorage',
     });
 
-    const isCallback = authContext.isCallback(window.location.hash);
+    const isCallback = authContext.isCallback(window.location.hash)
+
     if (isCallback) {
       authContext.handleWindowCallback();
-      getService();
     } else {
       authContext.login();
     }
 
-
-    function getService() {
-      let serviceToken = null;
-      authContext.acquireToken(serviceClientId, function (err, res) {
-        serviceToken = res;
-      });
+    let serviceToken = null;
+    authContext.acquireToken(this.state.serviceClientId, (err, res) => {
+      serviceToken = res;
       console.log(serviceToken);
-      const r = new XMLHttpRequest();
-      r.open('GET', serviceUrl, true);
-      r.setRequestHeader('Authorization', 'Bearer ' + serviceToken);
-      r.onreadystatechange = function () {
-        if (r.status === 200 && r.readyState === 4)
-          console.log(r.response);
-      };
-      r.send();
-    }
+    });
 
+    const r = new XMLHttpRequest();
+    r.open('GET', this.state.serviceUrl, true);
+    r.setRequestHeader('Authorization', 'Bearer ' + serviceToken);
+    r.onreadystatechange = function () {
+      if (r.status === 200 && r.readyState === 4)
+        console.log(r.response);
+    };
+    r.send();
   }
+
+  
 
   render() {
     return (
