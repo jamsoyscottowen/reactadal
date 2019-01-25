810b325da40a2070b87262c7e9f018851f759da0
Heri Utomo <heri.utomo@asabri.co.id>
2019-01-25 23:36:54 +0700

    Update Lagi

--- src/App.js
+++ src/App.js
@@ -3,42 +3,47 @@ import logo from './logo.svg';
 import { AuthenticationContext } from 'react-adal';
 import './App.css';
 
+export const adalConfig = {
+  aadTenant: 'asabri.co.id',
+  spaClientId: '1e957e0a-8aa7-4f77-91d7-27d25be9909b', //AAD app client id for this app
+  serviceClientId: '1fd046c7-83dd-4c06-bbff-546ff92232b0', //AAD app client id for the service API app
+  serviceUrl: 'http://api-hrms.asabri.co.id/api/pegawai/324', // the service API endpointa
+}
+
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      aadTenant: 'asabri.co.id',
-      spaClientId: '1e957e0a-8aa7-4f77-91d7-27d25be9909b', //AAD app client id for this app
-      serviceClientId: '1fd046c7-83dd-4c06-bbff-546ff92232b0', //AAD app client id for the service API app
-      serviceUrl: 'http://api-hrms.asabri.co.id/api/pegawai', // the service API endpointa
+      authContext: new AuthenticationContext({
+        instance: 'https://login.microsoftonline.com/',
+        tenant: adalConfig.aadTenant,
+        clientId: adalConfig.serviceClientId,
+        postLogoutRedirectUri: window.location.origin,
+        cacheLocation: 'localStorage',
+      }),
     }
+    this.state.authContext.handleWindowCallback();
   }
 
   componentDidMount() {
-    const authContext = new AuthenticationContext({
-      instance: 'https://login.microsoftonline.com/',
-      tenant: this.state.aadTenant,
-      clientId: this.state.serviceClientId,
-      postLogoutRedirectUri: window.location.origin,
-      cacheLocation: 'localStorage',
-    });
 
-    const isCallback = authContext.isCallback(window.location.hash)
-
-    if (isCallback) {
-      authContext.handleWindowCallback();
+    const user = this.state.authContext.getCachedUser();
+    if (!user) {
+      this.state.authContext.login();
     } else {
-      authContext.login();
+      this.getPegawaiService();
     }
+  }
 
+  getPegawaiService() {
     let serviceToken = null;
-    authContext.acquireToken(this.state.serviceClientId, (err, res) => {
+    this.state.authContext.acquireToken(adalConfig.serviceClientId, (err, res) => {
       serviceToken = res;
       console.log(serviceToken);
     });
 
     const r = new XMLHttpRequest();
-    r.open('GET', this.state.serviceUrl, true);
+    r.open('GET', adalConfig.serviceUrl, true);
     r.setRequestHeader('Authorization', 'Bearer ' + serviceToken);
     r.onreadystatechange = function () {
       if (r.status === 200 && r.readyState === 4)
@@ -47,7 +52,7 @@ class App extends Component {
     r.send();
   }
 
-  
+
 
   render() {
     return (
